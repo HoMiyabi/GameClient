a6d0ca59cf546043c02f697c117f34e573e5a945
鼠鼠 <37030246+HoMiyabi@users.noreply.github.com>
2024-09-14 14:24:07 +0800

    Update

--- Assets/Scripts/UI/KiraraTest.cs
+++ Assets/Scripts/UI/KiraraTest.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public partial class KiraraTest : MonoBehaviour
+namespace Kirara.Abc
 {
-    public int self;
-
-    // Start is called before the first frame update
-    void Start()
+    public partial class KiraraTest : MonoBehaviour
     {
-    }
+        public int self;
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
