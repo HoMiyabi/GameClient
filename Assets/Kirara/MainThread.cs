d0697dabdef7e2cfa958f8627bb9caee78745cd5
鼠鼠 <37030246+HoMiyabi@users.noreply.github.com>
2024-09-23 19:53:06 +0800

    Update

--- Assets/Kirara/MainThread.cs
+++ Assets/Kirara/MainThread.cs
@@ -5,16 +5,34 @@ namespace Kirara
 {
     public class MainThread : MonoSingleton<MainThread>
     {
+        private readonly int mainThreadId = Environment.CurrentManagedThreadId;
         private readonly ConcurrentQueue<Action> queue = new();
 
         public void Enqueue(Action action)
         {
-            queue.Enqueue(action);
+            if (action == null) return;
+
+            if (mainThreadId == Environment.CurrentManagedThreadId)
+            {
+                action();
+            }
+            else
+            {
+                queue.Enqueue(action);
+            }
         }
 
         private void Update()
         {
-            while (queue.TryDequeue(out Action action))
+            while (queue.TryDequeue(out var action))
+            {
+                action();
+            }
+        }
+
+        private void LateUpdate()
+        {
+            while (queue.TryDequeue(out var action))
             {
                 action();
             }
